7c4e4c2c7adf04fef8061102acd044bdd410f614
嘻嘻哈哈三角形\hwliv <hwlivy@gmail.com>
2023-11-30 19:14:36 +0800

    Map1&2

--- 1202.R
+++ 1202.R
@@ -8,3 +8,22 @@ boxplot(dat[,"eGFR"])
 pie(table(dat[,"Education"]))
 
 barplot(table(dat[,"Education"]))
+
+dat = read.csv("data10_1.csv", header = TRUE, fileEncoding = 'CP950')
+head(dat)
+
+library(RgoogleMaps)
+
+lat = c(22.88751, 23.41373)
+lon = c(120.023, 120.6562)
+center = c(mean(lat), mean(lon))
+zoom = min(MaxZoom(range(lat), range(lon)))
+
+
+MyMap = GetMap(center = center, zoom = zoom, API_console_key = 'AIzaSyA4DVFtF70aXE7RgrXViy2z5Ku2pMkVxFI')
+
+PlotOnStaticMap(MyMap)
+
+MyMap2 = GetMap(center = center, zoom = zoom, maptype = "satellite", API_console_key = 'AIzaSyA4DVFtF70aXE7RgrXViy2z5Ku2pMkVxFI')
+
+PlotOnStaticMap(MyMap2)
